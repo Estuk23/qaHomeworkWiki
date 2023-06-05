344c1687ce2effa1be379b3d617082b93ffa3816
unknown <kent.estupinan@gmail.com>
2023-06-04 19:06:08 -0600

    2.8 Homework part2

---
+++ 2.8/googleWBPHW.test.ts
@@ -0,0 +1,26 @@
+import {Google} from './basePageHW'
+import { Driver } from 'selenium-webdriver/chrome'
+const fs = require('fs')
+const google = new Google()
+
+test('do a search', async () => {
+    await google.navigate()
+    await google.search('Diablo 4')
+    let text = await google.getResults()
+    expect(text).toContain('Diablo 4')
+    await fs.writeFile(`${__dirname}/google.png`,
+     await google.driver.takeScreenshot(), "base64",
+     (e) => {
+         if (e) console.error(e)
+         else console.log('Save Succesful')
+     }
+     )
+    fs.writeFile(`${__dirname}/test.txt`, text, (e) => {
+        if (e) console.error(e)
+        else console.log('save Succesful')
+    })
+
+})
+afterAll(async () => {
+    await google.driver.quit()
+})
